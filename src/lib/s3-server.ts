9153824d4fcc9a4526f5dc08844998fa41ea8a39
Kevin Liao <kliao2016@gmail.com>
2023-11-20 21:30:59 -0700

    Presigned POST and GET work

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -5,28 +5,27 @@ import fs from "fs";
 export async function downloadFromS3(fileKey: string) {
     try {
         AWS.config.update({
-            accessKeyId: process.env.S3_ACCESS_KEY_ID,
-            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
         });
 
         const s3 = new AWS.S3({
             params: {
-                Bucket: process.env.S3_BUCKET_NAME,
+                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
             },
-            region: process.env.S3_REGION,
+            region: process.env.NEXT_PUBLIC_S3_REGION,
         });
 
         const presignParams = {
-            Bucket: process.env.S3_BUCKET_NAME!,
+            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
             Key: fileKey,
             Expires: 10,
-            ContentType: "application/pdf",
         }
 
         const presignedDownloadUrl = await s3.getSignedUrlPromise("getObject", presignParams);
 
         const config: AxiosRequestConfig = {
-            responseType: "blob",
+            responseType: "arraybuffer",
             onDownloadProgress: (event: AxiosProgressEvent) => {
                 console.log("Downloading from s3...", parseInt(((event.loaded * 100) / (event.total ?? 1)).toString()) + "%");
             }
--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -4,29 +4,32 @@ import AWS from "aws-sdk";
 export async function uploadToS3(file: File) {
     try {
         AWS.config.update({
-            accessKeyId: process.env.S3_ACCESS_KEY_ID,
-            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
         });
 
         const s3 = new AWS.S3({
             params: {
-                Bucket: process.env.S3_BUCKET_NAME,
+                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
             },
-            region: process.env.S3_REGION,
+            region: process.env.NEXT_PUBLIC_S3_REGION,
         });
 
         const fileKey = "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
 
         const presignParams = {
-            Bucket: process.env.S3_BUCKET_NAME!,
+            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
             Key: fileKey,
             Expires: 10,
             ContentType: "application/pdf",
-        }
+        };
 
         const presignedUploadUrl = await s3.getSignedUrlPromise("putObject", presignParams);
 
         const config = {
+            headers: {
+                'Content-Type': file.type
+            },
             onUploadProgress: (event: AxiosProgressEvent) => {
                 console.log("Uploading to s3...", parseInt(((event.loaded * 100) / (event.total ?? 1)).toString()) + "%");
             }
@@ -49,6 +52,6 @@ export async function uploadToS3(file: File) {
 }
 
 export function getS3Url(fileKey: string) {
-    const url = `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_S3_REGION}.amazonaws.com/${fileKey}`;
+    const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_S3_REGION}.amazonaws.com/${fileKey}`;
     return url;
 }
