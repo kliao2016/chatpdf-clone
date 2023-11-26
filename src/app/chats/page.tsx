28e1c158fda6f6dfdfb57ba0413e5106fe8fb42e
Kevin Liao <kliao2016@gmail.com>
2023-11-25 21:04:53 -0700

    Center text

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const AllChats = () => {
     return (
         <div className="w-full h-full bg-gradient-to-r from-indigo-300 to-purple-400">
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                <h1 className="text-5xl font-bold">
+            <div className="w-full h-full flex justify-center items-center">
+                <h1 className="text-5xl font-bold text-center">
                     Select a chat to see PDF and ask questions.
                 </h1>
             </div>
