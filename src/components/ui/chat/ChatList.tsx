1e6635b6b9344107823063dab601ae7ecc5d3674
Kevin Liao <kliao2016@gmail.com>
2023-12-09 15:47:27 -0700

    Use next router to opt dynamic routes out of spm and for faster navigation

--- src/components/ui/chat/ChatList.tsx
+++ src/components/ui/chat/ChatList.tsx
@@ -2,36 +2,32 @@
 import { DrizzleChat } from "@/lib/db/schema";
 import { cn } from "@/lib/utils";
 import { MessageCircle } from "lucide-react";
-import Link from "next/link";
-import { useParams } from "next/navigation";
-import React, { useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import React from "react";
 
 type Props = {
     chats: DrizzleChat[];
 };
 
 const ChatList = ({ chats }: Props) => {
-    const [activeChatId, setActiveChatId] = useState<string | null>(null);
+    let activeChatId: string = "0";
     const params = useParams();
-    if (
-        params.chatId !== undefined &&
-        typeof params.chatId === "string" &&
-        params.chatId !== activeChatId
-    ) {
-        setActiveChatId(params.chatId);
+    const router = useRouter();
+    if (params.chatId !== undefined && typeof params.chatId === "string") {
+        activeChatId = params.chatId;
     }
 
     return (
         <div className="f flex-col gap-2 mt-4">
             {chats.map((chat) => (
-                <Link
+                <button
+                    className="w-full"
                     key={chat.id}
-                    href={`/chats/${chat.id}`}
-                    onClick={() => setActiveChatId(chat.id.toString())}
+                    onClick={() => router.push(`/chats/${chat.id}`)}
                 >
                     <div
                         className={cn(
-                            "rounded-lg p-3 text-slate-300 flex items-center",
+                            "transition-all rounded-lg p-3 text-slate-300 flex items-center",
                             {
                                 "bg-blue-600 text-white":
                                     chat.id.toString() === activeChatId,
@@ -43,7 +39,7 @@ const ChatList = ({ chats }: Props) => {
                         <MessageCircle className="mr-2" />
                         <p>{chat.pdfName}</p>
                     </div>
-                </Link>
+                </button>
             ))}
         </div>
     );
